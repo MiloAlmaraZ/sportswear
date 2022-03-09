3b574ec25e66be32e2f5d3dca5ec2bfda36834c1
labtw14 <labtw14@labtw14-All-Series>
2022-03-09 09:59:16 -0600

    En esta seccion se subiran todos los archivos del proyecto segunda parte

--- Documentos/sportswear_listo/src/java/bd/Conexion.java
+++ Documentos/sportswear_listo/src/java/bd/Conexion.java
@@ -4,46 +4,96 @@
 package bd;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Conexion {
-private final Connection conexion;
-private final String url;
-private final String user;
-private final String pws;
+private  Connection conexion=null;
+private  String url="";
+private  String user="postgres";
+private  String pws="ramirez";
+ResultSet rs=null;
 public Conexion() {
     }
 
-    public final Connection getConexion() {
-        return conexion;
-    }
+//    public final Connection getConexion() {
+//        return conexion;
+//    }
+//
+//    public void setConexion(Connection conexion) {
+//         this.conexion = conexion;
+//    }
+//
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public void setUrl(String url) {
+//        this.url = url;
+//    }
+//
+//    public String getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(String user) {
+//        this.user = user;
+//    }
+//
+//    public String getPws() {
+//        return pws;
+//    }
+//
+//    public void setPws(String pws) {
+//        this.pws = pws;
+//    }
 
-    public void setConexion(Connection conexion) {
-         this.conexion = conexion;
-    }
 
-    public String getUrl() {
-        return url;
+public Connection conectaServer(){
+ 
+    try{
+        
+        Class.forName("org.postgresql.Driver");
+        conexion=DriverManager.getConnection("jdbc:postgresql://loclahost"+"/",user,pws);
+        System.out.println("Conecion establecida");
+    }catch(Exception ex){
+        System.out.println("Error:" + ex);
     }
+    
+    return conexion; 
+}
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+public Connection conectaBase(String bd){
+    try{
+        Class.forName("org.postgresql.Driver");
+        conexion=DriverManager.getConnection("jdbc:postgresql://loclahost"+"/"+bd,user,pws);
+        System.out.println("Conecion establecida");
+        
+    }catch(Exception ex){
+      System.out.println("Error:" + ex);
 
-    public String getUser() {
-        return user;
     }
+    return conexion;
+}
 
-    public void setUser(String user) {
-        this.user = user;
+public ResultSet Consultar(String sql, String bd){
+    
+    try{
+        ResultSet rs=null;
+        Connection con=null; 
+        con=conectaBase(bd);
+        Statement st =con.createStatement();
+        rs=st.executeQuery(sql);
+    }catch(Exception ex){
+        System.out.println("Error: "+ ex);
     }
-
-    public String getPws() {
-        return pws;
-    }
-
-    public void setPws(String pws) {
-        this.pws = pws;
-    }
+    
+    return rs;
+    
+    
+}
+//public v
 
 public static void  main(String[] args){
     
